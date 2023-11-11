269ec22a2e150c4874f7b3f9af00119d8de0bb11
ali <alicode0043@gmail.com>
2023-11-12 01:40:20 +0530

    add and get product  2:30

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { useState } from "react";
+import React, { useEffect, useState } from 'react'
 import MyContext from './myContext';
+import { fireDB } from '../../firebase/firebaseConfig';
+import { QuerySnapshot, Timestamp, addDoc, collection, onSnapshot, onSnapshotsInSync, orderBy, query } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 function MyState(props) {
   const [mode, setMode]= useState('light');
+  const [loading, setLoading] = useState(false);
 
   const toggleMode= ()=>{
     if (mode === 'light'){
@@ -16,12 +19,77 @@ function MyState(props) {
     }
   }
 
-  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString(
+      "en-US",
+      {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }
+    )
+  });
 
+ const addProduct =async ()=>{
+  if (products.title === null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+    return toast.error('Please fill all fields');
+  }
+  setLoading(true)
+  try {
+    const productRef = collection(fireDB, "products")
+    await addDoc(productRef, products)
+    toast.success("Product Add successfully")
+    getProductData()
+    closeModal()
+    setLoading(false)
+  } catch (error) {
+    console.log(error)
+    setLoading(false)
+  }
+  setProducts("")
+}
+    
+  
+  const [product,setProduct]=useState([]);
 
+  const getProductData = async ()=>{
+
+    setLoading(true);
+
+    try{
+      const q=query(
+        collection(fireDB, 'products'),
+        orderBy('times')
+      );
+      const data=onSnapshot(q, (QuerySnapshot)=>{
+        let productArray=[];
+        QuerySnapshot.forEach((doc)=>{
+          productArray.push({...doc.data(), id:doc.id});
+        });
+
+        setProduct(productArray);
+        setLoading(false);
+      });
+    }
+    catch(error){
+      console.log(error);
+      setLoading(false);
+    }
+    
+  }
+
+useEffect(()=>{
+  getProductData();
+},[]);
 
   return (
-    <MyContext.Provider value={ { toggleMode, mode, loading,setLoading }}>
+    <MyContext.Provider value={ { toggleMode, mode, loading,setLoading ,products,setProduct,addProduct,product}}>
        {props.children}
     </MyContext.Provider>
   )
